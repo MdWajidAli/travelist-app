6d8d781e7b258f6668f0e40c17cf4a1b10c792d8
Md Wajid Ali <106772487+MdWajidAli@users.noreply.github.com>
2025-02-03 04:54:37 +0530

    working with controlled elements of form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { use, useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -14,47 +16,60 @@ export default function App() {
   );
 }
 
-
-
 function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e)
+
+    if (!description) return;
+    
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem)
+
+    setQuantity(1);
+    setDescription("")
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}> 
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need fro your 😍 trip ?</h3>
-      <select>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map
-          ((num) => (
-            <option value={num} key={num}>{ num}</option>
+      <select value={quantity} onChange={(e) => (
+        setQuantity(Number(e.target.value)))}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
-
   );
 }
 function Packinglist() {
   return (
     <div className="list">
-    <ul>
-    {initialItems.map((item) => (
-      <Item item={item} key={item.id}/>
-    ))}
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
       </ul>
     </div>
-      );
+  );
 }
 
-function Item({item}) {
+function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
